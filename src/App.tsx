5ee2aa8ad0695192f201f50f4062406ea1da369b
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-13 06:27:26 +0600

    step 15 - add EsLint

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import './styles.css';
 const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Stater Template - {process.env.NODE_ENV} {process.env.name} </h1>
+      <h1>
+        React TypeScript Webpack Stater Template - {process.env.NODE_ENV}{' '}
+        {process.env.name}{' '}
+      </h1>
       <img src={Image} alt="React Logo" width="300" />
       <img src={Logo} alt="React Logo" width="300" />
     </>
-  )
-}
+  );
+};
 
 export default App;
