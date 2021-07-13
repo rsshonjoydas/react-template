a8968a658b14f53673a46ad19c72ad78d6c516e3
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-13 06:37:54 +0600

    step 16 - add Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Image from './images/logo.png';
-import Logo from './images/logo.svg';
-import './styles.css';
+import Image from "./images/logo.png";
+import Logo from "./images/logo.svg";
+import "./styles.css";
 
 const App = () => {
   return (
     <>
       <h1>
-        React TypeScript Webpack Stater Template - {process.env.NODE_ENV}{' '}
-        {process.env.name}{' '}
+        React TypeScript Webpack Stater Template - {process.env.NODE_ENV}{" "}
+        {process.env.name}{" "}
       </h1>
       <img src={Image} alt="React Logo" width="300" />
       <img src={Logo} alt="React Logo" width="300" />
